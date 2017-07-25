eb3ab5a83645ef080a77e262e9c57c853534340d
Abhilash Kumar <apravink@gmail.com>
2017-07-24 22:44:18 -0400

    Created a graph component with dummy data

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid} from 'recharts';
+
+
+
+const data = [
+    {name:'Groceries', Budgeted:400,Val:300},
+    {name:'Gas', Budgeted:200,Val:220},
+
+];
+
+const MonthlyChart = (props) =>{
+    return(
+       <BarChart width={600} height={300} data= {props.data}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <XAxis dataKey="name"/>
+            <YAxis/>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <Bar dataKey="Budgeted" fill="#8884d8" />
+            <Bar dataKey="Val" fill="#82ca9d" />
+            </BarChart>
+    )
+}
+
+
+// class MonthlyChart extends React.Component{
+//     render (){
+//         return(
+//             <div>
+//             <h1>Hi</h1>
+//             </div>
+//             // <BarChart width={600} height={300} data= {props.data}
+//             // margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+//             // <XAxis dataKey="name"/>
+//             // <YAxis/>
+//             // <Bar dataKey="Budgeted" fill="#8884d8" />
+//             // <Bar dataKey="Val" fill="#8884d8" />
+//             // </BarChart>
+//         )
+//     }
+// }
+
+
+
+
+
+ReactDOM.render(
+    <MonthlyChart data = {data}  />, document.getElementById('root'));
