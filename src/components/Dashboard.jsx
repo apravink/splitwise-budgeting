9f7f10df06413564917b5e52dc2b62b1f11961a1
Abhilash K <apravink@gmail.com>
2018-03-09 00:09:51 -0500

    WIP Need to add routing

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid } from "recharts";
+
+const data = [
+  { name: "Groceries", Budgeted: 400, Val: 300 },
+  { name: "Gas", Budgeted: 200, Val: 220 }
+];
+const MonthlyChart = props => {
+  return (
+    <BarChart
+      width={600}
+      height={300}
+      data={props.data}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid strokeDasharray="3 3" />
+      <Bar dataKey="Budgeted" fill="#8884d8" />
+      <Bar dataKey="Val" fill="#82ca9d" />
+    </BarChart>
+  );
+};
+export default class Dashboard extends React.Component {
+  state = {
+    isActive: ""
+  };
+  render() {
+    return (
+      <div>
+        <h1>Nov</h1>
+        <MonthlyChart data={data} />
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,42 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid} from 'recharts';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid } from "recharts";
 
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
+const Main = () => {
+  return (
+    <div>
+      <Login />
 
-const data = [
-    {name:'Groceries', Budgeted:400,Val:300},
-    {name:'Gas', Budgeted:200,Val:220},
+      <Dashboard />
+    </div>
+  );
+};
 
-];
-
-const MonthlyChart = (props) =>{
-    return(
-       <BarChart width={600} height={300} data= {props.data}
-            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-            <XAxis dataKey="name"/>
-            <YAxis/>
-            <CartesianGrid strokeDasharray="3 3"/>
-            <Bar dataKey="Budgeted" fill="#8884d8" />
-            <Bar dataKey="Val" fill="#82ca9d" />
-            </BarChart>
-    )
-}
-class MonthlyPanel extends React.Component{
-    state = {
-        isActive:'',
-    }
-    render(){
-        return(
-            <div>
-            <h1>Nov</h1>
-            <MonthlyChart data = {data} />
-            </div>
-        )
-    }
-}
-
-
-
-ReactDOM.render(
-    <MonthlyPanel  />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById("root"));
